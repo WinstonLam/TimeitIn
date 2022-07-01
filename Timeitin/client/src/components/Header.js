3f66e24e604395d024f67e69e5af3fd3296772a0
Winston Lam <winston.lam@outlook.com>
2022-07-01 10:15:01 +0200

    working on header

--- Timeitin/client/src/components/Header.js
+++ Timeitin/client/src/components/Header.js
@@ -1,114 +1,35 @@
 import * as React from "react";
-import {
-  AppBarStyled,
-  ContainerStyled,
-  TypographyStyled,
-} from "./Styles/Header";
+import BurgerMenu from "./BurgerMenu";
+import { Header, Wrapper, Name, NameMobile } from "./Styles/Header";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Typography from "@mui/material/Typography";
-
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
+import TimeitinLogo from "../images/TimeitinLogo.png";
 
 const pages = ["Create Account", "Settings"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBarStyled>
-      <ContainerStyled>
+    <Header>
+      <Wrapper>
         <Toolbar disableGutters>
-          <TypographyStyled variant="h6" component="a" noWrap href="/">
+          <Name variant="h6" component="a" noWrap href="/">
             Timeitin
-          </TypographyStyled>
-
-          <Div className="mobile-logo">
-            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-          </Div>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          </Name>
+          <Name variant="h6" component="a" noWrap href="/">
+            <img
+              src={TimeitinLogo}
+              alt="timeitin logo"
+              style={{ maxWidth: "40px" }}
+            />
+          </Name>
+          <BurgerMenu options={pages} />
           <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page}
@@ -116,8 +37,8 @@ const ResponsiveAppBar = () => {
             ))}
           </Box>
         </Toolbar>
-      </ContainerStyled>
-    </AppBarStyled>
+      </Wrapper>
+    </Header>
   );
 };
 export default ResponsiveAppBar;
--- Timeitin/client/src/components/Styles/Header.js
+++ Timeitin/client/src/components/Styles/Header.js
@@ -1,17 +1,18 @@
 import { AppBar, Container, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const AppBarStyled = styled(AppBar)({
+export const Header = styled(AppBar)({
   position: "static",
 });
 
-export const ContainerStyled = styled(Container)({
+export const Wrapper = styled(Container)({
   maxWidth: "xl",
 });
 
-export const TypographyStyled = styled(Typography)({
+export const Name = styled(Typography)({
   mr: 2,
   flexGrow: 2,
+  alignItems: "center",
   display: { xs: "none", md: "flex" },
   fontWeight: 700,
   letterSpacing: ".3rem",
@@ -19,4 +20,13 @@ export const TypographyStyled = styled(Typography)({
   textDecoration: "none",
 });
 
-export 
+export const NameMobile = styled(Typography)({
+  mr: 2,
+  display: { xs: "flex", md: "none" },
+  flexGrow: 1,
+  fontFamily: "monospace",
+  fontWeight: 700,
+  letterSpacing: ".3rem",
+  color: "inherit",
+  textDecoration: "none",
+});
